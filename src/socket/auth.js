e908ae466ff8a3983e7089ea23658b1632cd613c
Canaga81 <Canagamemmedov@gmail.com>
2024-09-02 22:06:55 +0400

    Chat App

---
+++ src/socket/auth.js
@@ -0,0 +1,17 @@
+function socketAuth(socket) {
+
+    socket.on('auth', (nickName) => {
+
+        socket.user = {
+
+            nickName,
+            
+        }
+
+        socket.join('group-chat');
+
+    })
+
+}
+
+module.exports = socketAuth;
